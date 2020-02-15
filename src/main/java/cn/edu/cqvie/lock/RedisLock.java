fcae2519c85c250ee0ca98eb184bf192eb396be1
382333604@qq.com <xxx@xxx.com>
2020-02-15 22:36:01 +0800

    redis lettuce 分布式锁，自旋超时时间计算

--- src/main/java/cn/edu/cqvie/lock/RedisLock.java
+++ src/main/java/cn/edu/cqvie/lock/RedisLock.java
@@ -11,26 +11,61 @@ package cn.edu.cqvie.lock;
  */
 public interface RedisLock {
 
-    long TIMEOUT_MILLIS = 5000;
+    /**
+     * 持有锁的默认最大超时时间 10s (单位毫秒)
+     */
+    long TIMEOUT_MILLIS = 10000;
 
+    /**
+     * 重试获取锁的retry最大超时时间 2s (单位毫秒)
+     */
     int RETRY_MILLIS = 2000;
 
+    /**
+     * 自旋获取锁的 sleep 时间
+     */
     long SLEEP_MILLIS = 10;
 
+    /**
+     * 获取锁 Redis 分布式锁，如果获取失败不重试获取
+     *
+     * @param key 获取锁的KEY
+     * @return
+     */
     boolean tryLock(String key);
 
+    /**
+     * 获取锁 Redis 分布式锁
+     *
+     * @param key 获取锁的KEY
+     * @return
+     */
     boolean lock(String key);
 
+    /**
+     * 获取锁 Redis 分布式锁
+     *
+     * @param key    获取锁的KEY
+     * @param expire 得到锁后最大持有时间，默认10s (单位毫秒)
+     * @return
+     */
     boolean lock(String key, long expire);
 
     /**
-     * 获取锁
-     * @param key 获取锁的KEY
-     * @param expire 得到锁后最大持有时间
-     * @param retryTimes 重试获取锁的retry最大时间
+     * 获取锁 Redis 分布式锁
+     *
+     * @param key        获取锁的KEY
+     * @param expire     得到锁后最大持有时间，默认10s (单位毫秒)
+     * @param retryTimes 重试获取锁的retry最大时间，默认2s  (单位毫秒)
      * @return
      */
     boolean lock(String key, long expire, long retryTimes);
 
+    /**
+     * 释放锁 Redis 分布式锁
+     *
+     * @param key 被释放锁的KEY
+     * @return
+     */
     boolean unlock(String key);
 }
